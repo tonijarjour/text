9b0d130dc3e64ee299c717825b68494bc8660a76
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2023-04-21 09:15:57 -0400

    splitting code up

--- src/editor.rs
+++ src/editor.rs
@@ -6,32 +6,49 @@ use std::io::{stdout, Write};
 pub struct Editor {}
 
 impl Editor {
-    pub fn run(&self) {
+    pub fn run() {
+        Self::setup();
+        Self::read_keys();
+        Self::exit();
+    }
+
+    fn read_keys() {
+        while let Ok(Event::Key(keycode)) = event::read() {
+            if Self::match_keycode(keycode).is_none() {
+                break;
+            }
+        }
+    }
+
+    fn match_keycode(keycode: KeyEvent) -> Option<()> {
+        match keycode {
+            KeyEvent {
+                code: KeyCode::Char(key),
+                modifiers: KeyModifiers::NONE,
+                ..
+            } => {
+                print!("{key}");
+                stdout().flush().unwrap();
+                Some(())
+            }
+            KeyEvent {
+                code: KeyCode::Char(key),
+                modifiers: KeyModifiers::CONTROL,
+                ..
+            } => match key {
+                'q' => None,
+                _ => Some(()),
+            },
+            _ => Some(()),
+        }
+    }
+
+    fn setup() {
         execute!(stdout(), crossterm::terminal::EnterAlternateScreen).unwrap();
         crossterm::terminal::enable_raw_mode().unwrap();
+    }
 
-        while let Ok(Event::Key(keycode)) = event::read() {
-                match keycode {
-                    KeyEvent {
-                        code: KeyCode::Char(key),
-                        modifiers: KeyModifiers::NONE,
-                        ..
-                    } => {
-                        print!("{key}");
-                        stdout().flush().unwrap()
-                    }
-                    KeyEvent {
-                        code: KeyCode::Char(key),
-                        modifiers: KeyModifiers::CONTROL,
-                        ..
-                    } => match key {
-                        'q' => break,
-                        _ => (),
-                    },
-                    _ => (),
-                }
-            }
-
+    fn exit() {
         execute!(stdout(), crossterm::terminal::LeaveAlternateScreen).ok();
         crossterm::terminal::disable_raw_mode().ok();
     }
