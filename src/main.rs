1b71d42db5b013e109a350e3f3cb7fb2f63d9798
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2023-10-08 20:20:10 -0400

    restart

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-mod document;
-mod editor;
-mod terminal;
-use editor::Editor;
-use std::env;
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut editor = if args.len() > 1 {
-        let file_path = &args[1];
-        Editor::from(file_path.as_str())
-    } else {
-        Editor::default()
-    };
-
-    editor.run();
+    println!("Hello, world!");
 }
